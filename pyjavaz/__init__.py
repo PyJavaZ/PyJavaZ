0149fbdae0dc87773816f061aa6401681d5a3313
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2024-04-02 09:43:07 -0700

    expose deserialize fn

--- pyjavaz/__init__.py
+++ pyjavaz/__init__.py
@@ -1,2 +1,3 @@
 from pyjavaz.wrappers import JavaObject, JavaClass, PullSocket, PushSocket
 from pyjavaz.wrappers import DEFAULT_BRIDGE_PORT, DEFAULT_BRIDGE_TIMEOUT
+from pyjavaz.bridge import deserialize_array
